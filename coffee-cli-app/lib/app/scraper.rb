64c00852d75c1f7b417075df82f6ab84500cb0c5
Eric Chrobak <eric@ericchrobak.com>
2018-08-01 01:45:43 +0000

    Changed the loop to start at index 1 instead of looping through the first index and having and if statement to control for that. The 1st index is just a header so we don't want that creating a new Roast.

--- coffee-cli-app/lib/app/scraper.rb
+++ coffee-cli-app/lib/app/scraper.rb
@@ -5,16 +5,16 @@ class Coffee::Scraper
     doc = Nokogiri::HTML(open(url))
     table = doc.css(".wikitable")[1]
     table_rows = table.css("tr")
-    table_rows.each_with_index do |a, index|
-      if index > 0
-        roast = Coffee::Roast.new
-        roast.name = a.css("th").text.chomp
-        roast.common_roast_names = a.css("td")[0].text
-        roast.notes = a.css("td")[1].text
-        roast.surface = a.css("td")[2].text
-        roast.flavor = a.css("td")[3].text
+    table_rows[1..3].each do |a, index|
 
-      end
+      roast = Coffee::Roast.new
+      roast.name = a.css("th").text.chomp
+      roast.common_roast_names = a.css("td")[0].text
+      roast.notes = a.css("td")[1].text
+      roast.surface = a.css("td")[2].text
+      roast.flavor = a.css("td")[3].text
+
+
     end
 
   end
