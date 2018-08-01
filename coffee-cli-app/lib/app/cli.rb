20be4dd791414a1fb06760b813269b80bcfda493
Eric Chrobak <eric@ericchrobak.com>
2018-08-01 02:16:08 +0000

    Added comments to cli.rb

--- coffee-cli-app/lib/app/cli.rb
+++ coffee-cli-app/lib/app/cli.rb
@@ -1,16 +1,20 @@
 # Our cLi controller
 class Coffee::CLI
 
+  # This method will call the other methods in the app
   def call
     puts "Looking for information about coffee roasts?"
     puts "Type yes or no"
     input = gets.strip.downcase
+    # If user types yes then the app continues and presents the options
+    # and then begins scraping wikipedia and creating Coffee::Roast objects
     if input == "yes"
       list_coffee
       menu
     end
   end
 
+  # Method to list the options the user has
   def list_coffee
 
     puts "Choose which roast you would like to learn more about:"
@@ -21,18 +25,28 @@ class Coffee::CLI
     DOC
   end
 
+  # Method to set the url, call the scraper class, receive the Roast methods,
+  # receive input, output the user's choice, the logic to determine what to
+  # output to the user.
   def menu
     input = nil
+    # Page being scraped
     url = "https://en.wikipedia.org/wiki/Coffee_roasting"
+    # This will scrape the wikipedia page and create the objects
     Coffee::Scraper.scrape_wikipedia(url)
+    # Retrieves the Roast objects from the Roast class
     @roasts = Coffee::Roast.all
+
+    # This will continue to receive input until the user types exit.
     while input != "Exit"
 
       input = gets.strip.capitalize
-
+      # This logic determines what to do with the user input
       case input
+        # output details of roast object based on input
       when "Dark", "Medium", "Light"
         puts "-"*30
+        # find the roast that matches the user input and retrieve
         output = @roasts.detect { |i|
           i.name == input
         }
